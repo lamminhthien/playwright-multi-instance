ed1122cafcfa7a786492a31226d2fed664c3ea98
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-11-02 13:22:11 +0700

    Fix timeout againg

--- index.ts
+++ index.ts
@@ -1,37 +1,32 @@
 import { chromium } from 'playwright';
 
-const url = 'https://www.quizne.com/room/qmfn0/multiple-play';
+const url = 'https://www.quizne.com/room/vk349/multiple-play';
 
 (async () => {
-  const numberOfInstances = 10; // Adjust the number of Chrome instances you want
+  const numberOfInstances = 20; // Adjust the number of Chrome instances you want
 
   const browserPromises = [];
 
   for (let i = 0; i < numberOfInstances; i++) {
-    browserPromises.push(chromium.launch({ headless: true, timeout: 9999999 }));
+    browserPromises.push(chromium.launch({ headless: true, timeout: 0 }));
   }
 
   const browsers = await Promise.all(browserPromises);
   for (const browser of browsers) {
     const page = await browser.newPage();
     await page.goto(url);
-      await page.getByPlaceholder('Type your name here').fill('a')
-      await page.keyboard.down('Enter');
-      console.log('Skip this error chrome instance');
-    console.log(`Page title: ${await page.title()}`);
+    await page.getByPlaceholder('Type your name here').fill('a')
+    await page.keyboard.down('Enter');
+    await page.setDefaultTimeout(0)
     console.log('Ready for play')
     // Perform actions for each browser instance
-    setTimeout(() => {
-      for (let i = 0; i < 350; i++) {
-        try {
-          console.log(i);
-          const randomAnswer = Math.floor(Math.random() * 4);
-          page.locator(`//p[contains(@class, 'my-auto break-words')]`).nth(randomAnswer).click();
-        } catch (error) {
-          console.log(`Nothing ${JSON.stringify(error)}`);
-        }
-      }
-    },20000)
+    // setTimeout(() => {
+    //   for (let i = 0; i < Infinity; i++) {
+    //       console.log(i);
+    //       const randomAnswer = Math.floor(Math.random() * 4);
+    //       page.locator(`//p[contains(@class, 'my-auto break-words')]`).nth(randomAnswer).click();
+    //   }
+    // },20000)
   }
 
 })();
