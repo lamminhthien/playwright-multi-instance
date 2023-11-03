c1c70da6285f3f4697beb2c0b0df0c862633c79b
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-11-03 10:11:40 +0700

    Improve promise run browser

--- index.ts
+++ index.ts
@@ -1,32 +1,46 @@
 import { chromium } from 'playwright';
 
-const url = 'https://www.quizne.com/room/vk349/multiple-play';
+const url = 'https://www.quizne.com/room/fdqqk/multiple-play';
+function splitArray<T>(array: T[], childArraySize: number): T[][] {
+  const splittedArray: T[][] = [];
+
+  for (let i = 0; i < array.length; i += childArraySize) {
+      const childArray = array.slice(i, i + childArraySize);
+      splittedArray.push(childArray);
+  }
+
+  return splittedArray;
+}
 
 (async () => {
-  const numberOfInstances = 20; // Adjust the number of Chrome instances you want
+  const numberOfInstances = 80; // Adjust the number of Chrome instances you want
+  const numberUserJoinSameTime = 10; // Adjust the number of Chrome instances you want
 
-  const browserPromises = [];
-
-  for (let i = 0; i < numberOfInstances; i++) {
-    browserPromises.push(chromium.launch({ headless: true, timeout: 0 }));
-  }
+  const instances = [...Array.from({length:numberOfInstances}).keys()].map((_,i)=>`ThienLam ${i+1}`)
+  const browserPromises = instances.map((e,i)=>chromium.launch({ headless: true, timeout: 0 }));
 
   const browsers = await Promise.all(browserPromises);
-  for (const browser of browsers) {
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.getByPlaceholder('Type your name here').fill('a')
-    await page.keyboard.down('Enter');
-    await page.setDefaultTimeout(0)
-    console.log('Ready for play')
-    // Perform actions for each browser instance
-    // setTimeout(() => {
-    //   for (let i = 0; i < Infinity; i++) {
-    //       console.log(i);
-    //       const randomAnswer = Math.floor(Math.random() * 4);
-    //       page.locator(`//p[contains(@class, 'my-auto break-words')]`).nth(randomAnswer).click();
-    //   }
-    // },20000)
+  console.log("🚀 Load chrome success: ", browsers.length)
+
+  const groupBrowsers = splitArray(browsers, 10);
+
+  for (let index = 0; index < groupBrowsers.length; index++) {
+    const groupBrowser = groupBrowsers[index];
+    const promiseGroupBrowserJoinRoom = groupBrowser.map(async (browser,i)=>{
+      const joinRoom = async ()=>{
+       const page = await browser.newPage();
+       await page.goto(url);
+       const userName = instances[index*numberUserJoinSameTime+i]
+       await page.getByPlaceholder('Type your name here').fill(userName)
+       await page.keyboard.down('Enter');
+       page.setDefaultTimeout(0)
+       console.log(`User ${userName} joined`)
+      }
+
+      return joinRoom()
+     })
+     await Promise.all(promiseGroupBrowserJoinRoom)
   }
+ 
 
 })();
