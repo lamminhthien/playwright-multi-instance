f35cc07f3d800f28479d002689a0bb9f9f94f760
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-03-24 16:02:50 +0700

    Add faker js to random name

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { chromium } from 'playwright';
+import { faker } from '@faker-js/faker';
 
 const url = 'https://www.quizne.com/room/w0orw/multiple-play';
 function splitArray<T>(array: T[], childArraySize: number): T[][] {
@@ -15,7 +16,7 @@ function splitArray<T>(array: T[], childArraySize: number): T[][] {
 (async () => {
   const numberOfInstances = 30; // Adjust the number of Chrome instances you want
   const numberUserJoinSameTime = 5; // Adjust the number of Chrome instances you want
-  const name = 'Thien Lam'
+  const name = faker.internet.userName();
 
   const instances = [...Array.from({length:numberOfInstances}).keys()].map((_,i)=>`${name} ${i+1}`)
   const browserPromises = instances.map((e,i)=>chromium.launch({ headless: true, timeout: 0 }));
